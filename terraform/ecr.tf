cf05ff40bd4cbce874b6f96dbd71dc3607519ea2
satoshi <satoshi@satoshinoMacBook-puro.local>
2023-12-07 08:26:58 +0900

    Update workflow

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,14 +3,3 @@ resource "aws_ecr_repository" "main" {
   name = "ai-typology"
   force_delete = true
 }
-
-# docker build & push
-resource "null_resource" "default" {
-  triggers = {
-    file_content_sha1 = sha1(join("", [for f in ["build-docker.sh", "../Dockerfile", "../front/Dockerfile"]: filesha1(f)]))
-  }
-
-  provisioner "local-exec" {
-    command = "sh ./build-docker.sh"
-  }
-}
